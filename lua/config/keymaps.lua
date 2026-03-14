59ce74cab84464aef83006bc09c95d40f2dec531
caleskog <christoffer.aleskog@gmail.com>
2026-03-14 12:35:47 +0100

    quality of life things

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+-- vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+vim.api.nvim_set_keymap("x", "<leader>p", '"_dP', { noremap = false, desc = "Paste without yank" })
---
+++ lua/plugins/core.lua
@@ -0,0 +1,36 @@
+return {
+  -- Added due to `vim` not being recognised
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    cmd = "LazyDev",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "LazyVim", words = { "LazyVim" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "lazy.nvim", words = { "LazyVim" } },
+      },
+    },
+  },
+
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+      },
+    },
+  },
+}
