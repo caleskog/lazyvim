2420b14fdfc7f35cd03fdffd23ee0b90d0a2e0c7
caleskog <christoffer.aleskog@gmail.com>
2026-03-14 14:30:35 +0100

    keymap for opening a terminal in a new buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,12 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Keymaps are automatically loaded on the VeryLazy event Default keymaps that
+-- are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
 -- vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 
 vim.api.nvim_set_keymap("x", "<leader>p", '"_dP', { noremap = false, desc = "Paste without yank" })
+
+vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], { noremap = false, silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>t", ":enew | terminal<CR>", { noremap = false, silent = true })
