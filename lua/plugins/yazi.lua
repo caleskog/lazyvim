d18bbef1f88991bab062e1ac632aef601c7c679f
caleskog <christoffer.aleskog@gmail.com>
2026-06-21 23:29:45 +0200

    allow yazi to open in LazyVim's root (root of project)

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,3 +1,12 @@
+---@module 'yazi'
+---@type YaziConfig | {}
+local yazi_opts = {
+  open_for_directories = true,
+  keymaps = {
+    show_help = "~",
+  },
+}
+
 return {
   ---@module 'lazy'
   ---@type LazySpec
@@ -13,12 +22,19 @@ return {
         "<leader>e",
         mode = { "n", "v" },
         "<cmd>Yazi<cr>",
-        desc = "Open yazi (current file)",
+        desc = "Open yazi (directory of current file)",
       },
       {
         "<leader>E",
         "<cmd>Yazi cwd<cr>",
         desc = "Open yazi (cwd)",
+      },
+      {
+        "<leader>fm",
+        function()
+          require("yazi").yazi(yazi_opts, LazyVim.root(), {})
+        end,
+        desc = "Open yazi (root)",
       },
       {
         "<leader>fy",
@@ -28,16 +44,9 @@ return {
     },
     ---@module 'yazi'
     ---@type YaziConfig | {}
-    opts = {
-      open_for_directories = true,
-      keymaps = {
-        show_help = "~",
-      },
-    },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
+    opts = yazi_opts,
     init = function()
       -- mark netrw as loaded so it's not loaded at all.
-      --
       -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
       vim.g.loaded_netrwPlugin = 1
     end,
