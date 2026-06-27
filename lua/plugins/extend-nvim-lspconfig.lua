6151178e9b4c61a6ff5bd04245ecdc4464966114
caleskog <christoffer.aleskog@gmail.com>
2026-06-27 12:54:05 +0200

    disable LSP progress and indexing notifications

--- lua/plugins/extend-nvim-lspconfig.lua
+++ lua/plugins/extend-nvim-lspconfig.lua
@@ -1,6 +1,10 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      -- Disable all LSP progress/indexing notifications
+      vim.lsp.handlers["$/progress"] = function() end
+    end,
     opts = {
       inlay_hints = { enabled = false },
       diagnostics = {
